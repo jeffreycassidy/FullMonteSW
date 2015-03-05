3dfbf44ef6d265dfb1dcefcb5c5bc043f0ef3aab
Jeff Cassidy <jeffrey.cassidy@gmail.com>
2015-03-05 17:06:43 -0500

    Removed old sandbox files

--- sandbox/randgen.cpp
+++
@@ -1,6 +0,0 @@
-#include <algorithm>
-#include <vector>
-
-int main(int argc,char **argv)
-{
-}
--- sandbox/setzero.cpp
+++
@@ -1,8 +0,0 @@
-#include <mmintrin.h>
-#include <emmintrin.h>
-
-int main(int argc,char **argv)
-{
-    __m128 m=_mm_setzero_ps();
-    return 0;
-}
