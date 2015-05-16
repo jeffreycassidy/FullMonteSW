8c708d2d36beffd0c6bf90e17ffa2a89ac595205
Jeffrey Cassidy <jeffrey.cassidy@gmail.com>
2015-05-16 10:24:36 -0400

    Added STL binary read and text write

---
+++ Storage/STL/STL.cpp
@@ -0,0 +1,60 @@
+#include "VTKHelpers.hpp"
+
+#include <boost/range/adaptor/map.hpp>
+#include <boost/range.hpp>
+#include <FullMonte/Storage/STL/STLBinary.hpp>
+
+using namespace std;
+
+int main(int argc,char **argv)
+{
+	string fn,ofn;
+	if (argc < 2)
+	{
+		cerr << "Error: filename required" << endl;
+		return -1;
+	}
+	else
+		fn = argv[1];
+
+	if (argc < 3)
+	{
+		cout << "INFO: Default output file name" << endl;
+		ofn = "mesh.text.stl";
+	}
+	else
+		ofn = argv[2];
+
+	cout << "Reading from binary STL " << fn << " and saving to text STL " << ofn << endl;
+
+	vector<array<float,3>> P;
+	vector<array<unsigned,3>> T;
+
+	tie(P,T) = loadSTLBinary(argv[1]);
+
+	vtkPoints *pts = vtkPoints::New();
+	boost::for_each(P, vtk_point_inserter(pts));
+
+	vtkCellArray *ca = vtkCellArray::New();
+	boost::for_each(T, vtk_cell_inserter(ca));
+
+	vtkUnsignedCharArray * r = vtkUnsignedCharArray::New();
+	r->SetNumberOfTuples(T.size());
+
+	auto __r = T | boost::adaptors::indexed(0U);
+
+	for(auto it=begin(__r); it !=end(__r); ++it)
+	{
+	}
+
+	vtkPolyData *pd = vtkPolyData::New();
+
+	pd->SetPoints(pts);
+	pd->SetPolys(ca);
+
+
+	write_vtk_file("from_stl.vtk","converted from STL",pd);
+
+	writeSTLText(ofn,P,T,"phantom");
+
+}
---
+++ Storage/STL/STLText.hpp
@@ -0,0 +1,30 @@
+/*
+ * STLText.hpp
+ *
+ *  Created on: May 16, 2015
+ *      Author: jcassidy
+ */
+
+#ifndef STORAGE_STL_STLTEXT_HPP_
+#define STORAGE_STL_STLTEXT_HPP_
+
+#include <string>
+#include <vector>
+#include <array>
+
+namespace STL {
+namespace Text {
+
+// to be implemented
+// void std::pair<std::vector<std::array<float,3>>,std::vector<std::array<unsigned,3>>> readSTLText
+
+void writeSTLText(std::string fn,
+		const std::vector<std::array<float,3>>& P,
+		const std::vector<std::array<unsigned,3>>& T,
+		std::string name="");
+
+}
+}
+
+
+#endif /* STORAGE_STL_STLTEXT_HPP_ */
