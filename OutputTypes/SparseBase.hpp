8b063814817c293e8fa76c96528aea08f7a54c58
Jeffrey Cassidy <jeffrey.cassidy@gmail.com>
2016-03-23 16:42:29 -0400

    Added new VTK bindings

---
+++ OutputTypes/SparseBase.hpp
@@ -0,0 +1,13 @@
+/*
+ * SparseBase.hpp
+ *
+ *  Created on: Mar 23, 2016
+ *      Author: jcassidy
+ */
+
+#ifndef OUTPUTTYPES_SPARSEBASE_HPP_
+#define OUTPUTTYPES_SPARSEBASE_HPP_
+
+template<typename T>bool nonzero(T x){ return x != T(); }
+
+#endif /* OUTPUTTYPES_SPARSEBASE_HPP_ */
